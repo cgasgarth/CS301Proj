5a3321fb31e6d6d0a7f609452b035820622418b9
cgasgarth <cgasgarth@gmail.com>
2021-09-19 17:31:43 -0400

    Final changes

--- Compiler/FunctionsTester.cpp
+++ Compiler/FunctionsTester.cpp
@@ -9,7 +9,7 @@ int main(int argc, char** argv){
     Converter c;
     vector<string> out;
     vector<string> lineOut;
-    lineOut = c.lineTakeIn("add $zero, $zero, $zero");
+    lineOut = c.lineTakeIn("addi $v0, $v1, -5");
     out.insert(out.end(), lineOut.begin(), lineOut.end());
     for (vector<string>::iterator t = out.begin(); t != out.end(); ++t) 
     {
