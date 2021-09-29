a88199b67988033e1d9e02f0b4574908df29ffce
cgasgarth <cgasgarth@gmail.com>
2021-09-28 20:26:54 -0400

    proj1 changes

--- Compiler/MIPScode/proj1test.asm
+++ Compiler/MIPScode/proj1test.asm
@@ -1,9 +1,5 @@
-          .data #memoized DP calculation of f(x) = f(x-1) + f(x//2) + x^2
-          .text #f(0) = 0
-          .align 2
-          .globl main
-main:
-  addi $a0, $zero, 30 #a0 = n
+main: 
+addi $a0, $zero, 30 #a0 = n #X
   addi $t0, $a0, 1
   sll $t0, $t0, 2
   sub $sp, $sp, $t0 #create an array of size n+1
@@ -12,16 +8,16 @@ main:
   add $t0, $a0, $zero #number of times to iterate
   addi $t1, $a1, 4 #pointer to iterate through array
   addi $t8, $zero, -1
-initloop:
-  sw $t8, 0($t1)
+initloop: 
+sw $t8, 0($t1)
   addi $t1, $t1, 4
   addi $t0, $t0, -1
-  bne $t0, $zero, initloop #init array to -1
-endinit:
-  jal f
-  j end
-f:
-  addi $sp, $sp, -16
+  bne $t0, $zero, initloop #init array to -1 #X
+endinit:  
+jal f 
+  j end 
+f:  
+addi $sp, $sp, -16
   sw $s0, 0($sp)
   sw $s1, 4($sp)
   sw $s2, 8($sp)
@@ -31,23 +27,23 @@ f:
   sll $t1, $t0, 2
   add $t1, $a1, $t1
   lw $s1, 0($t1)
-  slt $t2, $s1, $zero
-  beq $t2, $zero, firstcalldone
+  slt $t2, $s1, $zero 
+  beq $t2, $zero, firstcalldone #X
   addi $a0, $t0, 0
-  jal f
+  jal f 
   addi $s1, $v0, 0
-firstcalldone:
-  srl $t0, $s0, 1
+firstcalldone:  
+srl $t0, $s0, 1
   sll $t1, $t0, 2
   add $t1, $a1, $t1
   lw $s2, 0($t1)
-  slt $t2, $s2, $zero
-  beq $t2, $zero, secondcalldone
+  slt $t2, $s2, $zero #X
+  beq $t2, $zero, secondcalldone #X
   addi $a0, $t0, 0
-  jal f
+  jal f #X
   addi $s2, $v0, 0
-secondcalldone:
-  mult $s0, $s0
+secondcalldone: 
+mult $s0, $s0
   mflo $v0
   add $v0, $v0, $s1
   add $v0, $v0, $s2
@@ -60,6 +56,6 @@ secondcalldone:
   lw $ra, 12($sp)
   addi $sp, $sp, 16
   jr $ra
-end:
-  addi $v0, $zero, 10
-  syscall #End the program
+end:  
+addi $v0, $zero, 10 
+  syscall #End the program 
