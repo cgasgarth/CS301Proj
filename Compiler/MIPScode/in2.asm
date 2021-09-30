ac3cb39479f95dc69956533511fd71479c9bf044
cgasgarth <cgasgarth@gmail.com>
2021-09-30 15:53:51 -0400

    works?

--- Compiler/MIPScode/in2.asm
+++ Compiler/MIPScode/in2.asm
@@ -1,10 +1,2 @@
-.text
-.globl main
-main: 
-addi $t0, $zero, 5 #comment
-#comment
-addi $s0, $zero, 0
-loop:
-add $s0, $s0, $t0
-addi $t0, $t0, -1
-bne $t0, $zero, loop
+add $s1, $s2, $s5
+addi $s1, $s2, 242
