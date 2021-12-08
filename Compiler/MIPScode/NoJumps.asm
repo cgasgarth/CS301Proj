f95c54b477270c0854ffad25e76b2688e4250b89
cgasgarth <cgasgarth@gmail.com>
2021-12-08 15:37:33 -0500

    git ignore plz

--- Compiler/MIPScode/NoJumps.asm
+++
@@ -1,1 +0,0 @@
-jalr $s0, $s1 #load value from s0, store pc in s1
--- Compiler/MIPScode/in2.asm
+++
@@ -1,6 +0,0 @@
-addi $s1, $zero, 32 # address to write to terminal
-addi $s2, $zero, 115 # ascii for s
-sw $s2, 0($s1)
-j end
-end: 
-j end
